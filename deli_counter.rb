abef7196ae990820cfae17c8d0f3bee6641a77c2
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 16:30:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-#katz_deli =["Logan", "Avi", "Spencer"]
+katz_deli =["Logan", "Avi", "Spencer"]
 katz_deli = []
 def line(array)
   if array.length == 0
@@ -18,4 +18,4 @@ def now_serving(array)
 
 end
 
-#line(katz_deli)
+line(katz_deli)
